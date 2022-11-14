f557e8ca3218df807fb7b1ee541491e8d57e6977
Sayuri2323 <117921120+Sayuri2323@users.noreply.github.com>
2022-11-14 13:30:24 +0100

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,13 @@
+package it.develhope.es2;
+
+public class Team {
+    public String teamName;
+    public String name;
+    public String programmingLanguage;
+    public int yearsOfExperience;
+
+    public String printTeamDetails() {
+        return "Il nome è: " + name + ", il linguaggio conosciuto è: " + programmingLanguage;
+
+    }
+}
